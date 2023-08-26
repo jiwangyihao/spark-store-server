4eb21d0e38583a93d9f464dfbc434363530f92f6
jiwangyihao <jwyh@jwyihao.top>
2023-08-26 22:32:59 +0800

    修正：重新添加 CORS 支持

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'https://www.spark-app.store', //正式渠道
+      'http://localhost:9000', //本地测试
+      'https://spark.jwyihao.top', //Vercel 实时构建
+      'https://jiwangyihao.github.io', //GitHub Pages 实时构建
+      'https://deepin-community-store.gitee.io', //Gitee Pages 实时构建
+    ],
+  });
   await app.listen(3000);
 }
 bootstrap();
